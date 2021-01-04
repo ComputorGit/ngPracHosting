3f6dadc01350ecba1e656bcca4493f526bee473c
openSourceContro <torqueplayer@gmail.com>
2021-01-04 10:51:17 +0530

    update to deploy to heruko

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/ng-prac-hosting'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/angular-on-heroku/index.html'));});
+    '/dist/ng-prac-hosting/index.html'));});
 app.listen(process.env.PORT || 8080);
 
 console.log("Running")
