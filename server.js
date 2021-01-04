a876d1f2695f11b60ee4f3fb3a2a9c8db785684d
openSourceContro <torqueplayer@gmail.com>
2021-01-04 10:42:53 +0530

    update to deploy to heruko

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/angular-on-heroku/index.html'));});
 app.listen(process.env.PORT || 8080);
+
+console.log("Running")
