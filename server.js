8ad8ccf7ccd757bb904574a60caba7528aecbe97
BhaskarDixit <bhaskardixit786@gmail.com>
2021-01-04 11:15:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/ng-prac-hosting'));
 app.get('/*', function(req,res) {
-//   res.sendFile(path.join(__dirname+
-//     'ng-prac-hosting','index.html'));});
-
-res.sendFile(path.join(__dirname+'index.html'));});
+  res.sendFile(path.join(__dirname+
+    '/dist/ng-prac-hosting/index.html'));});
 
 app.listen(process.env.PORT || 8080);
 
